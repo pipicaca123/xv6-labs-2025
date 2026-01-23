492baf3539a6209edbe4efcfa97995959724f186
yicheng.lou <yicheng.lou@tronfuture.com>
2026-01-23 17:05:43 +0000

    child copy interpose_mask from parent in fork flow

--- kernel/proc.c
+++ kernel/proc.c
@@ -277,6 +277,9 @@ kfork(void)
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
 
+  // copy interpose_mask
+  np->interpose_mask = p->interpose_mask;
+
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
